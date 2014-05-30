62b3ae0d14696d43ece1dc1b962f6737bb0ae0d0
Griffin Ashe <gashe5363@gmail.com>
2014-05-30 11:56:50 -0400

    Update reverse.py

--- text/reverse.py
+++ text/reverse.py
@@ -3,4 +3,4 @@ Reverse a String - Enter a string and the program
 will reverse it and print it out.
 """
 
-print raw_input("This will be reversed. ")[::-1]
+print raw_input("Type something to reverse. ")[::-1]
