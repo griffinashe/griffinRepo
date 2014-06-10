20d455761e355870428f20bc480bfaef8503c2ff
Griffin Ashe <gashe5363@gmail.com>
2014-06-10 19:55:28 -0400

    Update product_inventory.py

--- Classes_projects/product_inventory.py
+++ Classes_projects/product_inventory.py
@@ -4,4 +4,3 @@ Create a product class which has a price, id, and quantity on hand.
 Then create an inventory class which keeps track of various products and 
 can sum up the inventory.
 """
-print "Griffin"
