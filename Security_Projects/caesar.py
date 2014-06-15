4047865c068092f123900cb1ad71eb0b731cc74f
Griffin Ashe <gashe5363@gmail.com>
2014-06-15 13:52:42 -0400

    Update caesar.py

--- Security_Projects/caesar.py
+++ Security_Projects/caesar.py
@@ -1,8 +1,12 @@
 """
-Caesar cipher - Implement a Caesar cipher, both encoding and decoding. 
+Caesar cipher - Implement a Caesar cipher. 
 The key is an integer from 1 to 25. This cipher rotates the letters 
 of the alphabet (A to Z). The encoding replaces each letter with the 
-1st to 25th next letter in the alphabet (wrapping Z to A). So key 2 encrypts "HI" to "JK", but key 20 encrypts "HI" to "BC". This simple "monoalphabetic substitution cipher" provides almost no security, because an attacker who has the encoded message can either use frequency analysis to guess the key, or just try all 25 keys
+1st to 25th next letter in the alphabet (wrapping Z to A). 
+So key 2 encrypts "HI" to "JK", but key 20 encrypts "HI" to "BC". 
+This simple "monoalphabetic substitution cipher" provides almost no security, 
+because an attacker who has the encoded message can either use frequency 
+analysis to guess the key, or just try all 25 keys.
 """
 
 import string
@@ -10,8 +14,10 @@ import string
 message = input("Enter the message: ")
 shift = int(input("Enter the shift: "))
 
+#create shifted alphabet
 shifted_alpha = string.ascii_lowercase[shift:] + string.ascii_lowercase[:shift]
 
+#create translation table
 trans_table = str.maketrans(string.ascii_lowercase, shifted_alpha)
 
 print(message.translate(trans_table))
